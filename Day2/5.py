9f9e0a65f84679aed1247f72ed67adf78528b82d
Rayan Das <rayandas91@gmail.com>
2019-10-23 12:58:55 +0530

    added 5.py

---
+++ Day2/5.py
@@ -0,0 +1,11 @@
+class rayan():
+    def __init__(self):
+        pass
+    def getString(self):
+        self.s = input()
+    def printString(self):
+        print(self.s.upper())
+
+p = rayan()
+p.getString()
+p.printString()
