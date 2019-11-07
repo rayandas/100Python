653f021c5ba2aa6360857ae5c35241d74c6cc4be
Rayan Das <rayandas91@gmail.com>
2019-11-07 16:35:40 +0530

    added 23.py

---
+++ Day8/23.py
@@ -0,0 +1,12 @@
+'''
+Write a method which can calculate square value of number.
+'''
+
+class Test:
+    def sqr(self,x):
+        return (x**2)
+
+n = int(input())
+xx = Test()
+num = xx.sqr(n)
+print(num)
