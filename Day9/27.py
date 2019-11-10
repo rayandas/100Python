eb1bec9c6f0e0e3cc283b786c626178155d1f196
Rayan Das <rayandas91@gmail.com>
2019-11-10 17:11:42 +0530

    added 27.py

---
+++ Day9/27.py
@@ -0,0 +1,11 @@
+'''
+Define a function that can convert a integer into a string and print it in console.
+'''
+
+conv = lambda x : str(x)
+
+n = conv(int(input("enter the integer, we'll convert the int to string: ")))
+print(n)
+print("the type of n is:")
+print(type(n))
+
