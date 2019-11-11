62ca2f1c3e74f167f2b697dc6ff5c2b9e72c0fb5
Rayan Das <rayandas91@gmail.com>
2019-11-11 13:44:17 +0530

    added 29.py

---
+++ Day9/29.py
@@ -0,0 +1,11 @@
+'''
+Define a function that can accept two strings as input and concatenate them and then print it in console.
+'''
+
+fun = lambda s1,s2 : s1+s2
+
+
+s1 = input()
+s2 = input()
+print(fun(s1,s2))
+
