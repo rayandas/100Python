ab1893d11dabb35513796da52e4e54cc176f326d
Rayan Das <rayandas91@gmail.com>
2019-11-18 12:17:57 +0530

    added 33.py

---
+++ Day10/33.py
@@ -0,0 +1,12 @@
+'''
+Define a function which can generate and print a list where the values are square of numbers between 1 and 20 (both included).
+'''
+def newlist():
+    lst = []
+    for i in range(1,21):
+        lst.append(i**2)
+    
+    print(lst)
+
+newlist()    
+
