88f00ac7cd37de3ae41e18a30f01566ab851a2b7
Rayan Das <rayandas91@gmail.com>
2019-11-20 13:32:20 +0530

    added 41.py

---
+++ Day11/41.py
@@ -0,0 +1,8 @@
+'''
+Write a program which can map() to make a list whose elements are square of elements in [1,2,3,4,5,6,7,8,9,10].
+'''
+
+li = [1,2,3,4,5,6,7,8,9,10]
+sqr = map(lambda x:x**2, li)
+print(list(sqr))
+
