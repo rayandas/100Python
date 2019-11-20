6efa344536012c0eb65ba99fa7fcc0b33a292385
Rayan Das <rayandas91@gmail.com>
2019-11-20 13:06:18 +0530

    added 40.py

---
+++ Day11/40.py
@@ -0,0 +1,10 @@
+'''
+Write a program which accepts a string as input to print "Yes" if the string is "yes" or "YES" or "Yes", otherwise print "No".
+'''
+
+s = input()
+if s.upper()=='YES':
+    print('Yes')
+else:
+    print('No')
+
