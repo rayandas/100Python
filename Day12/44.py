e296b5fe19638387f567fd649fc295be8e46f24f
Rayan Das <rayandas91@gmail.com>
2019-11-21 13:21:11 +0530

    added 44.py

---
+++ Day12/44.py
@@ -0,0 +1,8 @@
+'''
+Write a program which can map() to make a list whose elements are square of numbers between 1 and 20 (both included).
+'''
+
+num = map(lambda x:x*x, range(1,21))    
+
+print(list(num))
+
