ffe49e2c227ab636cec50e399bea9cdc0572ae5b
Rayan Das <rayandas91@gmail.com>
2019-11-22 19:57:21 +0530

    added 45.py

---
+++ Day12/45.py
@@ -0,0 +1,11 @@
+'''
+Define a class named American which has a static method called printNationality.
+'''
+
+class Indian():
+    def printNationality():
+        print("Indian")
+
+Indian.printNationality()        
+
+
