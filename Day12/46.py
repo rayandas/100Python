09c1a31448e1ffddd769aa4dd61deddddf3c9d87
Rayan Das <rayandas91@gmail.com>
2019-11-22 20:03:11 +0530

    added 46.py

---
+++ Day12/46.py
@@ -0,0 +1,16 @@
+'''
+Define a class named American and its subclass NewYorker.
+'''
+
+class Indian():
+    pass
+
+class Bengaluru(Indian):
+    pass
+
+india = Indian()
+bangalore = Bengaluru()
+
+print(india)
+print(bangalore)
+
